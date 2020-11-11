83db4b4df91ad193495f6151e5b626c4e27d07e5
DESKTOP-G0BRGEV\zhu60 <zhu603809832@163.com>
2020-11-11 23:16:09 +0800

    尝试编译glfw和glew和使用，不报link错误

--- LearnOpenGL/LearnOpenGL/main.cpp
+++ LearnOpenGL/LearnOpenGL/main.cpp
@@ -4,6 +4,12 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+void key_callback(GLFWwindow* pWindow, int nKey, int nScanCode, int nAction, int nMode)
+{
+	if (nKey == GLFW_KEY_ESCAPE && nAction == GLFW_PRESS)
+		glfwSetWindowShouldClose(pWindow, GL_TRUE);
+}
+
 int main()
 {
 	int nWidth = 0;
@@ -14,7 +20,14 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-	
+
+	if (GLEW_OK != glewInit())
+	{
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
 	GLFWwindow* pWindow = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
 	if (pWindow == nullptr)
 	{
@@ -22,13 +35,18 @@ int main()
 		glfwTerminate();
 		return -1;
 	}
-
+	glfwSetKeyCallback(pWindow, key_callback);
 	glfwMakeContextCurrent(pWindow);
 	glfwGetFramebufferSize(pWindow, &nWidth, &nHeight);
 	//glViewport(0, 0, nWidth, nHeight);
 	while (!glfwWindowShouldClose(pWindow))
 	{
+		//Event
 		glfwPollEvents();
+		//Render
+		//glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		//glClear(GL_COLOR_BUFFER_BIT);
+		//Swap
 		glfwSwapBuffers(pWindow);
 	}
 	glfwTerminate();
